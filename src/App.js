b7bd9a2556dc2a671ada504f5572431cf69f10d1
Abidsulaiman <abidqubit@gmail.com>
2020-10-06 12:05:15 +0530

    chrome extension using react

--- src/App.js
+++ src/App.js
@@ -16,10 +16,8 @@ class App extends Component {
           }
 
           <h1 className="App-title">Welcome to React</h1>
+          <p>To get started, edit <code>src/App.js</code> and save to reload.</p>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
